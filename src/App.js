5559397f3ed94c58e435d57fd8f8189e57c3c2b1
wangyiwei0108 <wangyiwei0108@gmail.com>
2021-01-31 21:09:57 +0800

    add redux to filter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
-import data from './data.json';
+// import data from './data.json';
 import store from './store';
 
 class App extends React.Component {
@@ -11,10 +11,10 @@ class App extends React.Component {
   constructor () {
     super ();
     this.state = {
-      products: data.products,
-      cartItems: JSON.parse(localStorage.getItem("cartItems")),
-      size: "",
-      sort: ""
+      // products: data.products,
+      cartItems: JSON.parse(localStorage.getItem("cartItems"))
+      // size: "",
+      // sort: ""
     }
   }
 
@@ -50,34 +50,34 @@ addToCart = (product) => {
   localStorage.setItem("cartItems", JSON.stringify(cartItems));
 }
 
-sortProducts = (event) => {
-  const sort = event.target.value;
-  this.setState((state) => ({
-  // 基於原來的 state 生成一個新的 state （下面將以 slice () shadow copy ）
-    sort: sort,
-    products: this.state.products.slice().sort((a, b) => (
-      sort === "lowest"?
-      ((a.price > b.price)? 1:-1):
-      sort === "highest"?
-      ((a.price < b.price)? 1:-1):
-      ((a._id > b._id)? 1:-1)
-    ))
-  }))
-}
+// sortProducts = (event) => {
+//   const sort = event.target.value;
+//   this.setState((state) => ({
+//   // 基於原來的 state 生成一個新的 state （下面將以 slice () shadow copy ）
+//     sort: sort,
+//     products: this.state.products.slice().sort((a, b) => (
+//       sort === "lowest"?
+//       ((a.price > b.price)? 1:-1):
+//       sort === "highest"?
+//       ((a.price < b.price)? 1:-1):
+//       ((a._id > b._id)? 1:-1)
+//     ))
+//   }))
+// }
 
-filterProducts = (event) => {
-  if (event.target.value === "") {
-    this.setState({
-      size: event.target.value, 
-      products: data.products});
-  } else {
-    this.setState({
-      size: event.target.value, 
-      products: data.products.filter((product)=> product.availableSizes.indexOf(event.target.value) >= 0
-      )
-    });
-  }
-};
+// filterProducts = (event) => {
+//   if (event.target.value === "") {
+//     this.setState({
+//       size: event.target.value, 
+//       products: data.products});
+//   } else {
+//     this.setState({
+//       size: event.target.value, 
+//       products: data.products.filter((product)=> product.availableSizes.indexOf(event.target.value) >= 0
+//       )
+//     });
+//   }
+// };
 
   render () {
     return (
@@ -90,14 +90,14 @@ filterProducts = (event) => {
             <div className="content">
               <div className="main">
                 <Filter 
-                count={this.state.products.length}
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={this.filterProducts}
-                sortProducts={this.sortProducts}
+                // count={this.state.products.length}
+                // size={this.state.size}
+                // sort={this.state.sort}
+                // filterProducts={this.filterProducts}
+                // sortProducts={this.sortProducts}
                 />
                 <Products 
-                productsX={this.state.products}
+                // products={this.state.products}
                 addToCart={this.addToCart}
                 />
               </div>
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { filterProducts, sortProducts } from "../actions/productActions";
 
-export default class Filter extends Component {
+class Filter extends Component {
     render() {
         return (
+            ! this.props.filteredProducts?
+            (<div>Loading...</div>) 
+            :
+            (
             <div className="filter">
-                <div className="filer-result">{this.props.count} Products</div>
+                <div className="filer-result">{this.props.filteredProducts.length} Products</div>
                 <div className="filter-sort">
                     Order {" "}
-                    <select value={this.props.sort} onChange={this.props.sortProducts}>
-                        <option>Latest</option>
+                    <select value={this.props.sort} onChange={(e) => this.props.sortProducts(this.props.filteredProducts, e.target.value)}>
+                        <option valie="latest">Latest</option>
                         <option value="lowest">Lowest</option>
                         <option value="highest">Highest</option>
                     </select>
                 </div>
                 <div className="filter-size">
                     Filter {" "}
-                    <select value={this.props.size} onChange={this.props.filterProducts}>
+                    <select value={this.props.size} onChange={(e) => this.props.filterProducts(this.props.products, e.target.value)}>
                         <option value="">All</option>
                         <option value="XS">XS</option>
                         <option value="S">S</option>
@@ -26,6 +32,24 @@ export default class Filter extends Component {
                     </select>
                 </div>
             </div>
+            )
         )
     }
 }
+
+
+// filteredProducts: 因為是完整的名單，所以給 sort 使用（因為只影響排序）。
+// products：因為要做篩選，所以給 size 使用（因為影響項目的增減）
+
+export default connect (
+    (state) => ({
+    size: state.products.size,
+    sort: state.products.sort,
+    filteredProducts: state.products.filteredItems,
+    products: state.products.items,
+    }),
+    {
+        filterProducts,
+        sortProducts,
+    }
+)(Filter)
